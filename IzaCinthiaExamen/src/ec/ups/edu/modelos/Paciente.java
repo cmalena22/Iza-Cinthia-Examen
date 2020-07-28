17b4ddfc117cc148f3c1d256c56325dadfeb3a32
Cinthia Iza <cizac1@est.ups.edu.ec>
2020-07-28 09:47:31 -0500

    Inicio

---
+++ IzaCinthiaExamen/src/ec/ups/edu/modelos/Paciente.java
@@ -0,0 +1,21 @@
+package ec.ups.edu.modelos;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Paciente
+ *
+ */
+@Entity
+
+public class Paciente implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Paciente() {
+		super();
+	}
+   
+}
